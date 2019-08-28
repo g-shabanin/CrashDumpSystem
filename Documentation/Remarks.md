7318f05ae77ad1bc02457d6aa73f05a979acd43e
g-shabanin <g.shabanin@gmail.com>
2019-08-28 21:30:42 +0300

    Added article Sergey Vaulin    Dumper-example-article-of-Sergey-Vaulin.pdf

---
+++ Documentation/Remarks.md
@@ -0,0 +1,5 @@
+
+https://habr.com/en/post/89094/
+http://www.devowl.net/2017/05/minidump-minidumpwritedump-c.html
+https://habr.com/en/post/89094/
+https://habr.com/ru/post/89220/
---
+++ src/Dumper/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Dumper
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+    using Dumps;
+
+    internal static class Program
+    {
+        private static bool _stopThread;
+
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Directory.GetFiles(DumpUtils.DumpDirectory).ToList().ForEach(File.Delete);
+            CreateThread();
+            TestMethod();
+            _stopThread = true;
+        }
+
+        private static void CreateThread()
+        {
+            new Thread(() =>
+            {
+                while (!_stopThread)
+                {
+                    Console.WriteLine("while(true)");
+                }
+            })
+            {
+                Name = "While(true) thread"
+            }.Start();
+        }
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        private static void TestMethod()
+        {
+            var dateTime = DateTime.Now;
+            try
+            {
+                Console.WriteLine(dateTime);
+
+                int someDouble = 0;
+                someDouble = 10 / someDouble;
+            }
+            catch (Exception)
+            {
+                DumpUtils.WriteDump();
+            }
+        }
+    }
+}
